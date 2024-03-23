c4fb524dea7cbe6e2d72f76c383541d6af5af5e2
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-23 20:43:19 +1300

    added code to check if user deposit in NaN or less than 0

--- project.js
+++ project.js
@@ -7,8 +7,20 @@
 // 7. Play again
 
 // 1. Take users deposit - ask user to enter a number
+// run node project.js to use terminal like a python terminal
 
 const prompt = require('prompt-sync')()
 const deposit = () => {
-  const depositAmount = prompt()
+  const depositAmount = prompt('Enter a deposit amount: ')
+  //this works by converting number strings to numbers - if it gets words or letters you get NaN
+  const numberDepositAmount = parseFloat(depositAmount)
+
+  // this confirm thats if that input in NaN or less than zero it loops back and asks for a real number
+  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+    console.log(
+      'Whole dollar bets only! Try again. Enter your deposit amount: '
+    )
+  }
 }
+
+deposit()
