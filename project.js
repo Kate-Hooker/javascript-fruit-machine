3751d8e87e7e2f0a75b56b82fe7d0a0fe0e92413
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-23 21:20:06 +1300

    Added input for number of lines user wants to bet on, checked for NaN and put inside a loop that will only return when a 1 2 or 3 is input

--- project.js
+++ project.js
@@ -11,23 +11,35 @@
 
 const prompt = require('prompt-sync')()
 const deposit = () => {
-
   while (true) {
-  const depositAmount = prompt('Enter a deposit amount: ')
-  //this works by converting number strings to numbers - if it gets words or letters you get NaN
-  const numberDepositAmount = parseFloat(depositAmount)
+    const depositAmount = prompt('Enter a deposit amount: ')
+    //this works by converting number strings to numbers - if it gets words or letters you get NaN
+    const numberDepositAmount = parseFloat(depositAmount)
 
-  // this confirm thats if that input in NaN or less than zero it loops back and asks for a real number
-  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-    console.log(
-      'Whole dollar bets only! Try again. '
-    )} else {
-      return numberDepositAmount;
+    // this confirm thats if that input in NaN or less than zero it loops back and asks for a real number
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log('Whole dollar bets only! Try again. ')
+    } else {
+      return numberDepositAmount
     }
-  }
   }
 }
 
+// 2. Figure out how many lines they want to bet on
+const getNumberOfLines = () => {
+  const lines = prompt(
+    'How many lines would you like to bet one? Must be between 1 and 3: '
+  )
+  //this works by converting number strings to numbers - if it gets words or letters you get NaN
+  const numberOfLines = parseFloat(lines)
 
+  // this confirm thats if that input in NaN, smaller than 1 or bigger than 3 it loops back and asks for a real number
+  if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+    console.log('Please enter 1, 2 or 3! Try again. ')
+  } else {
+    return numberOfLines
+  }
+}
 
-deposit()
+const depositAmount = deposit()
+console.log('Youre balance is ' + depositAmount)
