6ab4134fa1267ffd2d6e749713a7abe88adbff41
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-23 20:23:17 +1300

    Psudocoded project.js covering basic outline

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Take users deposit
+// 2. How many lines do they want to bet on
+// 3. How big are they betting
+// 4. Randomise slot machine
+// 5. Check for win
+// 6. Add winnings
+// 7. Play again
